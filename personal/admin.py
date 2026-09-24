62654d70efed404827fc76d62ba9cd33bb4b9588
Romina De Vasquez <rominadevasquez@gmail.com>
2026-09-24 20:55:05 -0300

    configuracion de adm

--- personal/admin.py
+++ personal/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import Departamento, Cargo, Empleado
 
-# Register your models here.
+
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+    )
+
+    search_fields = (
+        'nombre',
+    )
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'nombre',
+    )
+
+    search_fields = (
+        'nombre',
+    )
+
+
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = (
+        'rut',
+        'nombre',
+        'apellido',
+        'cargo',
+        'departamento',
+        'estado',
+    )
+
+    search_fields = (
+        'rut',
+        'nombre',
+        'apellido',
+    )
+
+    list_filter = (
+        'departamento',
+        'cargo',
+        'estado',
+    )
+
+    ordering = (
+        'apellido',
+        'nombre',
+    )
+
